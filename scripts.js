51463f4fa29c1b05eaba54e71df8b2fa327cc4b8
Manikandan <91401060+Manikadan1801@users.noreply.github.com>
2024-07-14 13:44:50 +0530

    Create scripts.js
    new theme

---
+++ scripts.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+
+  // this for top nav bar
+  var divWidth = $('#nav').width();
+  if (divWidth > 960) {
+    $('#navp').addClass('p-fixed');
+  }
+
+  // this for work section tabs
+  $('.header-right-nav > a').click(function () {
+    document.querySelectorAll('.header-right-nav > a').forEach(function (item) {
+      item.classList.remove('active')
+    })
+    this.classList.add('active')
+  });
+
+
+  // this for progrees bar animation
+  function animateListItems() {
+    var widths = ['80%', '95%', '95%', '55%', '90%', '25%'];
+    $("li .progress-bar").each(function (index) {
+      var widthIndex = index % widths.length;
+      console.log(index);
+      $(this).animate({
+        width: widths[widthIndex]
+      }, 1000);
+    });
+  }
+
+  const observerConfig = {
+    rootMargin: '-50% 0% -50% 0%',
+    threshold: 0
+  }
+
+  let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        animateListItems();
+        textAnimation();
+        observer.disconnect();
+        console.log('intersecting')
+      }
+    });
+  }, observerConfig);
+
+  function textAnimation() {
+    $("p, h2, h1").css({
+      "opacity": "1",
+      "transition": "opacity 1s"
+    });
+  }
+
+
+  observer.observe($("#anim")[0]);
+
+});
