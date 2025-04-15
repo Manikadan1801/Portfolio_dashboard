e0736c31386bcfe6bf95a558f87476b31b853ad3
Manikandan Selvaraj <sdnani123@gmail.com>
2025-04-15 22:57:02 +0530

    Revert "15-apr commit for intro section"
    This reverts commit 3c52d05d0a110decb95cc0058eb903c7c57346b5.

--- scripts.js
+++ scripts.js
@@ -6,40 +6,24 @@ const modeSwitch = document.querySelector('.toggle-switch');
 const modeText = document.querySelector('.mode-text');
 const navLinks = document.querySelectorAll('.nav-links');
 
-
-// sidebar toggle
 toggle.addEventListener('click', () => {
     sidebar.classList.toggle('close');
 });
 
-// when clicked calling the mode change
 modeSwitch.addEventListener('click', () => {
- modeChange();
-});
-
-
-//  mode change function
- function modeChange() {
-    body.classList.toggle('dark');
+  body.classList.toggle('dark');
   if (body.classList.contains('dark')) {
       modeText.innerText = 'Dark mode';
       
   } else {
       modeText.innerText = 'Light mode';
   }
-}
+});
 
-// Add active class to the clicked link and remove from others
 navLinks.forEach(link => {
     console.log(link);
     link.addEventListener('click', () => {
         navLinks.forEach(l => l.classList.remove('active'));
         link.classList.add('active');
     });
-});
-
-
-// when screen loaded calling the mode change
-window.addEventListener('load', () => {
-    modeChange();
 });
